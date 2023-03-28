a0f82c59a4680f2e82fa11d091cd063cc334f8d3
1ambuj <ambuj83032@gmail.com>
2023-03-28 17:53:02 +0530

    update quizz app

--- Travel website/css/quiz app/quiz.js
+++ Travel website/css/quiz app/quiz.js
@@ -26,20 +26,61 @@ const questions = [
     
 ]
 let index = 0
+let total = questions.length
+let right = 0;
+let wrong = 0;
+let option_input = document.querySelectorAll(".option")
 let option_selector = document.querySelectorAll(".leb")
 //console.log(option_selector[0].textContent)
 let quest = document.querySelector(".ques")
 const question_declaration = ()=>{
+    if(index == total){
+        return endQuiz()
+    }
+    recet()
     const data = questions[index]
     quest.textContent = `${index + 1})${data.que}`
-    option_selector[0].textContent = data.a
-    option_selector[1].textContent = data.b
-    option_selector[2].textContent = data.c
-    option_selector[3].textContent = data.d 
+    option_input[0].nextElementSibling.textContent = data.a
+    option_input[1].nextElementSibling.textContent = data.b
+    option_input[2].nextElementSibling.textContent = data.c
+    option_input[3].nextElementSibling.textContent = data.d 
+  
 
 }
 const get_submit =() =>{
+    const data = questions[index]
+    const answ = get_answer()
+    //console.log(answ,data.correct)
+    if(answ==data.correct){
+        right++
+    }else{
+        wrong++
+    }
     index = index+1
+    question_declaration()
+    return;
+
 }
-get_submit()
+const get_answer = ()=>{
+    let answer; 
+    option_input.forEach(
+        (input)=>{
+              if(input.checked){
+                    console.log(input.value);
+              }
+    })
+    return answer;
+}
+const recet = ()=>{
+    option_input.forEach((input)=>{
+        input.checked= false
+    })
+}
+const endQuiz = () =>{
+    document.querySelector(".box").innerHTML =
+    `<h3>Thanks for playing quiz <h3>
+    <h2>${right}/${total} are correct </h2>
+    `
+}
+//get_submit()
 question_declaration()
